17c7cb289149dc2b5e8647b955271b02ebd85e1e
Aadi Pandey <aadipandey202005@gmail.com>
2025-10-27 11:53:59 +0530

    added map feature

--- seeds/index.js
+++ seeds/index.js
@@ -29,6 +29,13 @@ const seedDB = async () => {
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             price,
             images: [
                 {
